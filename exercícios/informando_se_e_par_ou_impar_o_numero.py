5c483636379d46fbda2a39db4a10991ad3b69a1d
Samuel Alves <alvess7221@gmail.com>
2024-11-13 15:48:31 -0300

    commit - 13/11/2024

--- exercícios/informando_se_e_par_ou_impar_o_numero.py
+++ exercícios/informando_se_e_par_ou_impar_o_numero.py
@@ -1,12 +1,10 @@
 entrada = input('Digite um número: ')
 try:
     numero_usr_int = int(entrada)
-    par_ou_impar = entrada
-    if entrada.isdigit():
-        numero_usr_int = int(entrada)
-        if numero_usr_int % 2 == 0:
-            print(f'O número que você digitou é par.')
-        else:
-            print(f'O número que você digitou é ímpar.')                    
+    par_impar = numero_usr_int % 2 == 0
+    par_impar_texto = 'impar'
+    if par_impar:
+        par_impar_texto = 'par'
+    print(f'O número {numero_usr_int} é {par_impar_texto}.')              
 except:
     print('Erro! Digite um número qualquer.')
